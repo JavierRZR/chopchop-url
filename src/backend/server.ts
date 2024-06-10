dcad251efe39ebdf083bfec63256b81fbf13c296
Javi <ruizromero98@gmail.com>
2024-06-10 09:51:43 +0200

    Fix; Deploy

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -371,7 +371,7 @@ app.post("/authorizeUrl", (req, res) => {
 });
 
 // Start the server
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
