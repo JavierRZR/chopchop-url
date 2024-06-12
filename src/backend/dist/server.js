c64ec7be66ce119858b3307e805be8828485b220
Javi <ruizromero98@gmail.com>
2024-06-12 14:49:09 +0200

    Fix

--- src/backend/dist/server.js
+++ src/backend/dist/server.js
@@ -5,7 +5,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 require("dotenv").config();
 const express_1 = __importDefault(require("express"));
-const express_session_1 = __importDefault(require("express-session"));
+// import session from "express-session";
+const cookie_session_1 = __importDefault(require("cookie-session"));
 const cookieParser = require("cookie-parser");
 const passport_1 = __importDefault(require("passport"));
 const cors_1 = __importDefault(require("cors"));
@@ -24,10 +25,8 @@ const corsOptions = {
 app.use((0, cors_1.default)(corsOptions));
 app.use(cookieParser());
 // Configure express-session middleware
-app.use((0, express_session_1.default)({
+app.use((0, cookie_session_1.default)({
     secret: process.env.SESSION_SECRET_KEY,
-    resave: false,
-    saveUninitialized: true,
 }));
 // Initialize passport middleware
 app.use(passport_1.default.initialize());
